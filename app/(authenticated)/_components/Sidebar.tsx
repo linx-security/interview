0eda45d8946a502dfc6d3148aac501e73cad8c9e
CodeSandbox <support@codesandbox.io>
2023-08-16 08:06:21 +0000

    Initial commit

---
+++ app/(authenticated)/_components/Sidebar.tsx
@@ -0,0 +1,55 @@
+'use client';
+import AccountSwitcher from './AccountSwitcher';
+import NavGroup from './NavGroup';
+import NavItem from './NavItem';
+
+import { Box, Circle, Flex, Stack } from '@chakra-ui/react';
+import {
+  BiAnalyse,
+  BiBookContent,
+  BiBuoy,
+  BiCog,
+  BiCommentAdd,
+  BiHome,
+  BiLink,
+  BiNews,
+  BiRuler
+} from 'react-icons/bi';
+
+const Sidebar = () => {
+    return (
+    <Box w="64" bg="gray.900" color="white" fontSize="sm">
+    <Flex h="full" direction="column" px="4" py="4">
+      <AccountSwitcher />
+      <Stack spacing="8" flex="1" overflow="auto" pt="8">
+        <Stack spacing="1">
+          <NavItem active icon={<BiHome />} label="Get Started" />
+          <NavItem icon={<BiCommentAdd />} label="Inbox" />
+        </Stack>
+        <NavGroup label="Integrations">
+          <NavItem icon={<BiLink />} label="Connections" />
+          <NavItem icon={<BiBookContent />} label="Complience" />
+        </NavGroup>
+
+        <NavGroup label="Insights">
+          <NavItem icon={<BiNews />} label="Reports" />
+          <NavItem icon={<BiAnalyse />} label="Automations" />
+          <NavItem icon={<BiRuler />} label="Rules" />
+        </NavGroup>
+      </Stack>
+      <Box>
+        <Stack spacing="1">
+          <NavItem subtle icon={<BiCog />} label="Settings" />
+          <NavItem
+            subtle
+            icon={<BiBuoy />}
+            label="Help & Support"
+            endElement={<Circle size="2" bg="purple.400" />}
+          />
+        </Stack>
+      </Box>
+    </Flex>
+  </Box>)
+}
+
+export default Sidebar
---
+++ app/(authenticated)/page.tsx
@@ -0,0 +1,27 @@
+
+'use client';
+import { Box, Flex,  useColorModeValue as mode } from '@chakra-ui/react'
+import Sidebar from './_components/Sidebar'
+import {Metadata} from 'next'
+ const App = () => {
+  return (
+    <Box height="100vh" overflow="hidden" position="relative">
+      <Flex h="full" id="app-container">
+       <Sidebar />
+        <Box bg={mode('#fafafa', 'gray.800')} flex="1" p="6">
+          <Box
+            w="full"
+            h="full"
+            rounded="lg"
+            border="3px dashed currentColor"
+            color={mode('gray.200', 'gray.700')}
+          />
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
+
+export default App;
+
+
